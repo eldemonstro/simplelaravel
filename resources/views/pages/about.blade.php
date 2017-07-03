500f792d12d018ffa59a9345ad6c989797885085
Fabricio Bezerra <demonstro003@gmail.com>
2017-07-02 21:07:43 -0300

    Laravel from Scratch part 4 done

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,1 +1,6 @@
-About
+@extends('layouts.app')
+@section('content')
+  <h1>{{ $title }}</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed doculpa qui
+    officia deserunt mollit anim id est laborum.</p>
+@endsection
