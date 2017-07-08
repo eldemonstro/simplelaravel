95cf514dff0869edaa296f55d88152cb0e3b8ea9
Fabricio Bezerra <demonstro003@gmail.com>
2017-07-08 01:40:21 -0300

    Laravel From Scratch [Part 9]

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,32 @@
-<!doctype html>
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
-        <title>{{config('app.name', 'LSAPP')}}</title>
-    </head>
-    <body>
-      @include('inc.navbar')
-      <div class="container">
-        @include('inc.messages')
-        @yield('content')
-      </div>
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-      <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
-      <script>
-        CKEDITOR.replace( 'article-ckeditor' );
-      </script>
-    </body>
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        @include('inc.navbar')
+        <div class="container">
+          @include('inc.messages')
+          @yield('content')
+        </div>
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+    <script>
+      CKEDITOR.replace( 'article-ckeditor' );
+    </script>
+</body>
 </html>
---
+++ resources/views/layouts/ohyeah.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <title>{{config('app.name', 'LSAPP')}}</title>
+    </head>
+    <body>
+      @include('inc.navbar')
+      <div class="container">
+        @include('inc.messages')
+        @yield('content')
+      </div>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+      <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+      <script>
+        CKEDITOR.replace( 'article-ckeditor' );
+      </script>
+    </body>
+</html>
